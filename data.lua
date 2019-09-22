b3eaf0cf4fe3c45a667e4a29baf082776b991f8c
Alan Baines <abaines@vt.edu>
2019-09-22 17:15:28 -0400

    search target_effects and code organization

--- data.lua
+++ data.lua
@@ -14,7 +14,7 @@ for k,v in pairs(data.raw["artillery-turret"]) do
 	log(k)
 end
 
-
+---------------------------------------------------------------------------------------------------
 
 local derpyArtilleryAmmo = table.deepcopy(data.raw["ammo"]["artillery-shell"])
 
@@ -24,6 +24,7 @@ derpyArtilleryAmmo.ammo_type.action.action_delivery.projectile="derpy-artillery-
 data:extend{derpyArtilleryAmmo}
 log(serpent.block(derpyArtilleryAmmo))
 
+---------------------------------------------------------------------------------------------------
 
 local derpyArtilleryShellRecipe = table.deepcopy(data.raw.recipe["artillery-shell"])
 
@@ -32,49 +33,34 @@ derpyArtilleryShellRecipe.result = "derpy-artillery-ammo"
 
 data:extend{derpyArtilleryShellRecipe}
 
+---------------------------------------------------------------------------------------------------
 
 local derpyArtilleryProjectile = table.deepcopy(data.raw["artillery-projectile"]["artillery-projectile"])
 
 derpyArtilleryProjectile.name = "derpy-artillery-projectile"
---local action = derpyArtilleryProjectile.action.action_delivery.target_effects.action
---[[
-local action = derpyArtilleryProjectile.action
-action.action_delivery.target_effects = {
-	{
-		type = "damage",
-		damage = {amount = 5 , type = "physical"}
-	},
-	{
-		type = "damage",
-		damage = {amount = 5 , type = "explosion"}
-	}
-}
-action.radius = 50
-]]--
 
 local target_effects = derpyArtilleryProjectile.action.action_delivery.target_effects
 
+local happy = 0
+
 log("for k,v in pairs(target_effects) do")
 log(#target_effects)
-for k,v in pairs(target_effects) do
-	log(k)
-	log(serpent.block(v))
+for index,target_effect in pairs(target_effects) do
+	if target_effect['action'] and target_effect['type']=="nested-result" then
+		local action = target_effect['action']
+		log(serpent.block(target_effect))
+		log(serpent.block(action))
+		happy = 1 + happy
+	end
+end
+
+if happy~=1 then
+	error("not happy")
 end
 
 data:extend{derpyArtilleryProjectile}
---log("serpent.block(derpyArtilleryProjectile.action)")
---log(serpent.block(derpyArtilleryProjectile.action))
 
---log("serpent.block(derpyArtilleryProjectile.action.action_delivery)")
---log(serpent.block(derpyArtilleryProjectile.action.action_delivery))
-
---log("serpent.block(derpyArtilleryProjectile.action.action_delivery.target_effects)")
---log(serpent.block(derpyArtilleryProjectile.action.action_delivery.target_effects))
-
---log("serpent.block(derpyArtilleryProjectile.action.action_delivery.target_effects.action)")
---log(serpent.block(derpyArtilleryProjectile.action.action_delivery.target_effects.action))
-
-
+---------------------------------------------------------------------------------------------------
 
 local derpyArtilleryGun = table.deepcopy(data.raw["gun"]["artillery-wagon-cannon"])
 
@@ -90,6 +76,7 @@ attack_parameters.ammo_category = "artillery-shell"
 
 data:extend{derpyArtilleryGun}
 
+---------------------------------------------------------------------------------------------------
 
 local derpyArtilleryEntity = table.deepcopy(data.raw["artillery-turret"]["artillery-turret"])
 
@@ -100,9 +87,9 @@ derpyArtilleryEntity.turret_rotation_speed = 0.0001
 derpyArtilleryEntity.turn_after_shooting_cooldown = 200
 derpyArtilleryEntity.gun = "derpy-artillery-gun"
 
-
 data:extend{derpyArtilleryEntity}
 
+---------------------------------------------------------------------------------------------------
 
 local derpyArtilleryItem = table.deepcopy(data.raw["item"]["artillery-turret"])
 
@@ -120,6 +107,7 @@ derpyArtilleryRecipe.result = "derpy-artillery"
 
 data:extend{derpyArtilleryRecipe}
 
+---------------------------------------------------------------------------------------------------
 
 local military2 = data.raw.technology["military-2"]
 
