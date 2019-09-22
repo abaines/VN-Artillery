f91dc3ef73d26b749521301980e88a37438c43ec
Alan Baines <abaines@vt.edu>
2019-09-22 13:40:17 -0400

    second...
    got an entity, item and recipe!!

---
+++ data.lua
@@ -0,0 +1,46 @@
+-- Kizrak
+
+--data.lua
+
+log("derpy data.lua")
+
+for k,v in pairs(data.raw) do
+	log(k)
+end
+
+log("derpy data.lua artillery-turret")
+
+for k,v in pairs(data.raw["artillery-turret"]) do
+	log(k)
+end
+
+
+
+
+
+local derpyArtilleryEntity = table.deepcopy(data.raw["artillery-turret"]["artillery-turret"])
+
+derpyArtilleryEntity.name = "derpy-artillery"
+derpyArtilleryEntity.order = "b[turret]-d[artillery-turret]"
+derpyArtilleryEntity.minable = {mining_time = 6, result = "derpy-artillery"},
+
+data:extend{derpyArtilleryEntity}
+
+
+local derpyArtilleryItem = table.deepcopy(data.raw["item"]["artillery-turret"])
+
+derpyArtilleryItem.name = "derpy-artillery"
+derpyArtilleryItem.place_result = "derpy-artillery"
+derpyArtilleryItem.stack_size = 1
+
+data:extend{derpyArtilleryItem}
+
+
+local derpyArtilleryRecipe = table.deepcopy(data.raw.recipe["artillery-turret"])
+
+derpyArtilleryRecipe.enabled = true
+derpyArtilleryRecipe.name = "derpy-artillery"
+derpyArtilleryRecipe.result = "derpy-artillery"
+
+data:extend{derpyArtilleryRecipe}
+
