4a687d7c6145030bf53b003ce2b8ee1550510de0
Alan Baines <abaines@vt.edu>
2019-09-28 12:22:50 -0400

    new technology research for light artillery

--- data.lua
+++ data.lua
@@ -153,7 +153,12 @@ data:extend{derpyArtilleryRecipe}
 
 ---------------------------------------------------------------------------------------------------
 
-local military2 = data.raw.technology["military-2"]
+local lightArtilleryResearch = table.deepcopy(data.raw.technology["military-2"])
+lightArtilleryResearch.name = "derpy-artillery"
+lightArtilleryResearch.icon = "__base__/graphics/technology/artillery.png"
+lightArtilleryResearch.unit.count = 5
+
+lightArtilleryResearch.effects = {}
 
 local unlock1 = {
 	type = "unlock-recipe",
@@ -165,6 +170,22 @@ local unlock2 = {
 	recipe = "derpy-artillery-ammo"
 }
 
-table.insert(military2.effects,unlock1)
-table.insert(military2.effects,unlock2)
+table.insert(lightArtilleryResearch.effects,unlock1)
+table.insert(lightArtilleryResearch.effects,unlock2)
+
+table.insert(lightArtilleryResearch.prerequisites,"fluid-handling")
+
+data:extend{lightArtilleryResearch}
+log(serpent.block(lightArtilleryResearch))
+
+
+---------------------------------------------------------------------------------------------------
+
+local artilleryResearch = data.raw.technology["artillery"]
+
+table.insert(artilleryResearch.prerequisites,"derpy-artillery")
+table.insert(artilleryResearch.prerequisites,"military-3")
+table.insert(artilleryResearch.prerequisites,"utility-science-pack")
+table.insert(artilleryResearch.prerequisites,"explosives")
+table.insert(artilleryResearch.prerequisites, "automobilism")
 
