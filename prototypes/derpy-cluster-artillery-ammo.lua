1596b172fdfbdd55c0560d59e99a388e8ca2ef62
Alan Baines <abaines@vt.edu>
2020-06-28 03:03:28 -0400

    Remove all "bonus" graphics !
    Remove final_action
    Remove create-trivial-smoke
    Remove create-entity
    Remove invoke-tile-trigger
    Remove destroy-decoratives

--- prototypes/derpy-cluster-artillery-ammo.lua
+++ prototypes/derpy-cluster-artillery-ammo.lua
@@ -2,6 +2,8 @@
 
 -- prototypes\derpy-cluster-artillery-ammo.lua
 
+local sb = serpent.block -- luacheck: ignore 211
+
 ---------------------------------------------------------------------------------------------------
 
 -- the recicpe to craft the item
@@ -37,6 +39,8 @@ local derpyArtilleryProjectile = table.deepcopy(data.raw["artillery-projectile"]
 
 derpyArtilleryProjectile.name = "derpy-cluster-artillery-projectile-1"
 
+derpyArtilleryProjectile.final_action = nil
+
 derpyArtilleryProjectile.action = {
    {
       type = "cluster",
@@ -50,25 +54,8 @@ derpyArtilleryProjectile.action = {
          direction_deviation = 0.6,
          starting_speed = 0.25,
          starting_speed_deviation = 0.3,
-         target_effects = {
-            {
-               initial_height = 0,
-               max_radius = 3.5,
-               offset_deviation = { { -4, -4 }, { 4, 4 } },
-               repeat_count = 240,
-               smoke_name = "artillery-smoke",
-               speed_from_center = 0.05,
-               speed_from_center_deviation = 0.005,
-               type = "create-trivial-smoke"
-            },
-            {
-               entity_name = "big-artillery-explosion",
-               type = "create-entity"
-            },
-         --   { scale = 0.25, type = "show-explosion-on-chart" }
-         }
       }
-   }
+   },
 }
 
 data:extend{derpyArtilleryProjectile}
@@ -83,39 +70,6 @@ local derpyClusterArtilleryProjectile =
       acceleration = 0.005,
       action =
       {
-         {
-            type = "direct",
-            action_delivery =
-            {
-               type = "instant",
-               target_effects =
-               {
-                  {
-                     type = "create-entity",
-                     entity_name = "grenade-explosion"
-                  },
-                  {
-                     type = "create-entity",
-                     entity_name = "big-scorchmark",
-                     check_buildability = true,
-                  },
-                  {
-                     type = "invoke-tile-trigger",
-                     repeat_count = 1,
-                  },
-                  {
-                     type = "destroy-decoratives",
-                     from_render_layer = "decorative",
-                     to_render_layer = "object",
-                     include_soft_decoratives = true, -- soft decoratives are decoratives with grows_through_rail_path = true
-                     include_decals = false,
-                     invoke_decorative_trigger = true,
-                     decoratives_with_trigger_only = false, -- if true, destroys only decoratives that have trigger_effect set
-                     radius = 2.25 -- large radius for demostrative purposes
-                  }
-               }
-            }
-         },
          {
             type = "cluster",
             cluster_count = 11,
@@ -176,10 +130,12 @@ local derpyClusterArtilleryPellet =
                      type = "damage",
                      damage = {amount = 1, type = "impact"}
                   },
+						--[[
                   {
                      type = "create-entity",
                      entity_name = "explosion"
                   }
+						]]--
                }
             }
          }
