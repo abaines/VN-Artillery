dc3b64478a4ab2ed738ce438d18c7271a0e574fa
Alan Baines <abaines@vt.edu>
2020-06-29 09:47:09 -0400

    whitespace formatting

--- prototypes/derpy-cluster-artillery-ammo.lua
+++ prototypes/derpy-cluster-artillery-ammo.lua
@@ -32,10 +32,10 @@ local derpyArtilleryShellRecipe = table.deepcopy(data.raw.recipe["artillery-shel
 derpyArtilleryShellRecipe.name = "derpy-cluster-artillery-ammo"
 derpyArtilleryShellRecipe.result = "derpy-cluster-artillery-ammo"
 derpyArtilleryShellRecipe.ingredients = {
-   {"derpy-artillery-ammo", 6},
-   {"advanced-circuit", 1},
-   {"explosives", 1},
-   {type="fluid", name="lubricant", amount=5}
+	{"derpy-artillery-ammo", 6},
+	{"advanced-circuit", 1},
+	{"explosives", 1},
+	{type="fluid", name="lubricant", amount=5}
 }
 derpyArtilleryShellRecipe.category = "advanced-crafting"
 derpyArtilleryShellRecipe.energy_required = 20
@@ -70,20 +70,20 @@ table.insert(target_effects,{
 })
 
 derpyArtilleryProjectile.action = {
-   {
-      type = "cluster",
-      cluster_count = 11,
-      distance = 10,
-      distance_deviation = 15,
-      action_delivery =
-      {
-         type = "projectile",
-         projectile = "cluster-projectile-2", -- not grenade
-         direction_deviation = 0.6,
-         starting_speed = 0.25,
-         starting_speed_deviation = 0.3,
-      }
-   },
+	{
+		type = "cluster",
+		cluster_count = 11,
+		distance = 10,
+		distance_deviation = 15,
+		action_delivery =
+		{
+			type = "projectile",
+			projectile = "cluster-projectile-2", -- not grenade
+			direction_deviation = 0.6,
+			starting_speed = 0.25,
+			starting_speed_deviation = 0.3,
+		}
+	},
 	{
 		type = "area",
 		radius = 2.5,
@@ -107,45 +107,45 @@ data:extend{derpyArtilleryProjectile}
 ---------------------------------------------------------------------------------------------------
 
 local derpyClusterArtilleryProjectile =
-   {
-      type = "projectile",
-      name = "cluster-projectile-2",
-      flags = {"not-on-map"},
-      acceleration = 0.005,
-      action =
-      {
-         {
-            type = "cluster",
-            cluster_count = 11,
-            distance = 10,
-            distance_deviation = 15,
-            action_delivery =
-            {
-               type = "projectile",
-               projectile = "cluster-artillery-pellet-3",
-               direction_deviation = 0.6,
-               starting_speed = 0.25,
-               starting_speed_deviation = 0.3,
-               max_range = 20,
-            }
-         }
-      },
-      light = {intensity = 0.5, size = 4},
-      animation =
-      {
-         filename = "__base__/graphics/entity/artillery-projectile/hr-shell.png",
-         width = 64,
-         height = 64,
-         scale = 0.5
-      },
-      shadow =
-      {
-         filename = "__base__/graphics/entity/artillery-projectile/hr-shell-shadow.png",
-         width = 64,
-         height = 64,
-         scale = 0.5
-      },
-   }
+	{
+		type = "projectile",
+		name = "cluster-projectile-2",
+		flags = {"not-on-map"},
+		acceleration = 0.005,
+		action =
+		{
+			{
+				type = "cluster",
+				cluster_count = 11,
+				distance = 10,
+				distance_deviation = 15,
+				action_delivery =
+				{
+					type = "projectile",
+					projectile = "cluster-artillery-pellet-3",
+					direction_deviation = 0.6,
+					starting_speed = 0.25,
+					starting_speed_deviation = 0.3,
+					max_range = 20,
+				}
+			}
+		},
+		light = {intensity = 0.5, size = 4},
+		animation =
+		{
+			filename = "__base__/graphics/entity/artillery-projectile/hr-shell.png",
+			width = 64,
+			height = 64,
+			scale = 0.5
+		},
+		shadow =
+		{
+			filename = "__base__/graphics/entity/artillery-projectile/hr-shell-shadow.png",
+			width = 64,
+			height = 64,
+			scale = 0.5
+		},
+	}
 
 log(sb( derpyClusterArtilleryProjectile ))
 data:extend{derpyClusterArtilleryProjectile}
@@ -153,44 +153,44 @@ data:extend{derpyClusterArtilleryProjectile}
 ---------------------------------------------------------------------------------------------------
 
 local derpyClusterArtilleryPellet =
-   {
-      type = "projectile",
-      name = "cluster-artillery-pellet-3",
-      direction_only = true,
-      flags = {"not-on-map"},
-      acceleration = 0.005,
-      action =
-      {
-         {
-            type = "area",
-            radius = 2.5,
-            action_delivery =
-            {
-               type = "instant",
-               target_effects =
-               {
-                  {
-                     type = "damage",
-                     damage = {amount = 6, type = "impact"}
-                  },
-                  {
-                     type = "create-entity",
-                     entity_name = "explosion"
-                  }
-               }
-            }
-         }
-      },
-      light = {intensity = 0.5, size = 4},
-      animation =
-      {
-         filename = "__base__/graphics/entity/bullet/bullet.png",
-         frame_count = 1,
-         width = 3,
-         height = 50,
-         priority = "high"
-      },
-   }
+	{
+		type = "projectile",
+		name = "cluster-artillery-pellet-3",
+		direction_only = true,
+		flags = {"not-on-map"},
+		acceleration = 0.005,
+		action =
+		{
+			{
+				type = "area",
+				radius = 2.5,
+				action_delivery =
+				{
+					type = "instant",
+					target_effects =
+					{
+						{
+							type = "damage",
+							damage = {amount = 6, type = "impact"}
+						},
+						{
+							type = "create-entity",
+							entity_name = "explosion"
+						}
+					}
+				}
+			}
+		},
+		light = {intensity = 0.5, size = 4},
+		animation =
+		{
+			filename = "__base__/graphics/entity/bullet/bullet.png",
+			frame_count = 1,
+			width = 3,
+			height = 50,
+			priority = "high"
+		},
+	}
 
 log(sb( derpyClusterArtilleryPellet ))
 data:extend{derpyClusterArtilleryPellet}
