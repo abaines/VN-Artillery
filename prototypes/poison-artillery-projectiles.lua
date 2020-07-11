1266f066b67353704d3b05ecac3ac8e36c1885e0
Alan Baines <abaines@vt.edu>
2020-07-11 04:00:45 -0400

    exploration & investigation 🔎

--- prototypes/poison-artillery-projectiles.lua
+++ prototypes/poison-artillery-projectiles.lua
@@ -6,3 +6,20 @@ local sb = serpent.block -- luacheck: ignore 211
 
 ---------------------------------------------------------------------------------------------------
 
+local artillery_projectile = table.deepcopy( data.raw["artillery-projectile"]["artillery-projectile"] )
+
+artillery_projectile.action = {}
+artillery_projectile.final_action = {}
+
+log(sb( artillery_projectile ))
+
+---------------------------------------------------------------------------------------------------
+
+log(sb( data.raw["capsule"]["poison-capsule"] ))
+
+log(sb( data.raw["projectile"]["poison-capsule"] ))
+
+log(sb( data.raw["smoke-with-trigger"]["poison-cloud"] ))
+
+---------------------------------------------------------------------------------------------------
+
