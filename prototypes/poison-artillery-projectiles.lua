cbb0972a72aa1332abc7ca73e7c4b54aa57231e4
Alan Baines <abaines@vt.edu>
2020-07-11 03:26:04 -0400

    Create poison-artillery-projectiles.lua

---
+++ prototypes/poison-artillery-projectiles.lua
@@ -0,0 +1,8 @@
+-- Kizrak
+
+-- \prototypes\poison-artillery-projectiles.lua
+
+local sb = serpent.block -- luacheck: ignore 211
+
+---------------------------------------------------------------------------------------------------
+
