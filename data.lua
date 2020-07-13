1c55a09b0f003fdc05bdacbbbb051dbf8474abfe
Alan Baines <abaines@vt.edu>
2020-07-12 20:10:13 -0400

    rename file

--- data.lua
+++ data.lua
@@ -4,7 +4,7 @@
 
 require("prototypes.derpy-artillery-ammo")
 require("prototypes.derpy-artillery-gun")
-require("prototypes.derpy-cluster-artillery-ammo")
+require("prototypes.derpy-cluster-artillery-weapon")
 require("prototypes.poison-artillery-misc")
 require("prototypes.poison-artillery-projectiles")
 require("prototypes.technology")
