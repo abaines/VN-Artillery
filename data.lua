31f6821b70003ac89e3f79d4f50817cd01aaac11
Alan Baines <abaines@vt.edu>
2020-07-12 20:14:56 -0400

    split (relocate) code

--- data.lua
+++ data.lua
@@ -4,6 +4,7 @@
 
 require("prototypes.derpy-artillery-ammo")
 require("prototypes.derpy-artillery-gun")
+require("prototypes.derpy-cluster-artillery-misc")
 require("prototypes.derpy-cluster-artillery-weapon")
 require("prototypes.poison-artillery-misc")
 require("prototypes.poison-artillery-projectiles")
