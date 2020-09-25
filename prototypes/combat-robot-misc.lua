ad99379b78500c2131db4834f0755b7a692f9bd6
Alan Baines <abaines@vt.edu>
2020-09-25 18:43:57 -0400

    template
    start of Combat Robot Artillery Projectile

---
+++ prototypes/combat-robot-misc.lua
@@ -0,0 +1,12 @@
+-- Kizrak
+
+-- prototypes\combat-robot-misc.lua
+
+local sb = serpent.block -- luacheck: ignore 211
+
+---------------------------------------------------------------------------------------------------
+
+-- item/shell/ammo
+-- research
+-- recipe
+
---
+++ prototypes/combat-robot-weapon.lua
@@ -0,0 +1,12 @@
+-- Kizrak
+
+-- prototypes\combat-robot-weapon.lua
+
+local sb = serpent.block -- luacheck: ignore 211
+
+---------------------------------------------------------------------------------------------------
+
+-- projectile/artillery-projectile
+-- damage/robot
+-- actions & triggers
+
