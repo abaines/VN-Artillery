2d488078fc46298d8d9ddfa19407a219b47c2e45
Alan Baines <abaines@vt.edu>
2020-09-27 14:34:12 -0400

    recipe

--- prototypes/combat-robot-misc.lua
+++ prototypes/combat-robot-misc.lua
@@ -18,4 +18,24 @@ ammo.ammo_type.action.action_delivery.projectile="la-combat-robot-projectile-1"
 ammo.stack_size = 10
 
 data:extend{ammo}
+log("ammo:\n"..sb(ammo))
+
+
+local recipe = {
+	enabled = false,
+	energy_required = 20,
+	ingredients = {
+		{"derpy-artillery-ammo",1},
+		{"defender-capsule",1},
+		{"repair-pack",1},
+		{type="fluid", name="water", amount=5},
+	},
+	category = "advanced-crafting",
+	name = "la-combat-robot-artillery-shell-recipe",
+	result = "la-combat-robot-artillery-shell",
+	type = "recipe"
+}
+
+data:extend{recipe}
+log("recipe:\n"..sb(recipe))
 
