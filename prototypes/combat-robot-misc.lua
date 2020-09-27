ce73fba55328b05cfa1bb788ad8994aa971c0921
Alan Baines <abaines@vt.edu>
2020-09-27 12:43:48 -0400

    just copy base

--- prototypes/combat-robot-misc.lua
+++ prototypes/combat-robot-misc.lua
@@ -10,3 +10,13 @@ local sb = serpent.block -- luacheck: ignore 211
 -- research
 -- recipe
 
+-- item the player carries
+local ammo = table.deepcopy(data.raw["ammo"]["artillery-shell"])
+
+ammo.name = "la-combat-robot-artillery-shell"
+ammo.ammo_type.action.action_delivery.projectile="la-combat-robot-projectile-1"
+ammo.stack_size = 10
+
+data:extend{ammo}
+log(sb(ammo))
+
--- prototypes/combat-robot-weapon.lua
+++ prototypes/combat-robot-weapon.lua
@@ -10,3 +10,11 @@ local sb = serpent.block -- luacheck: ignore 211
 -- damage/robot
 -- actions & triggers
 
+-- thing that flies through air and does damage
+local artillery_projectile = table.deepcopy(data.raw["artillery-projectile"]["artillery-projectile"])
+
+artillery_projectile.name = "la-combat-robot-projectile-1"
+
+data:extend{artillery_projectile}
+log(artillery_projectile)
+
