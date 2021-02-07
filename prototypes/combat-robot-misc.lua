b5ade3e586bc815f55a50461419f24f94dcea0aa
Alan Baines <abaines@vt.edu>
2021-02-07 15:12:27 -0500

    Back to 1.1 ----- again.......

--- prototypes/combat-robot-misc.lua
+++ prototypes/combat-robot-misc.lua
@@ -53,7 +53,7 @@ local technology = {
 	icon_size = 128,
 	name = "la-combat-robot-artillery-shell-research",
 	prerequisites = {
-		"combat-robotics", -- defender
+		"defender", -- combat-robotics
 		"derpy-artillery"
 	},
 	type = "technology",
