5262f6b8ed92851d59cafd28c9956c9da7b7e78c
MrDuckFIN <mutanenoskari@gmail.com>
2023-03-21 22:10:19 +0200

    Fixed syntax error in settings.lua

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,5 @@
 -- Apply settings
-local derpyArtilleryGun = data.raw["gun"]["derpy-artillery-gun"];
+local derpyArtilleryGun = data.raw["gun"]["derpy-artillery-gun"]
 derpyArtilleryGun.attack_parameters.min_range = settings.startup["derpy-artillery-min_range"].value
 derpyArtilleryGun.attack_parameters.range = settings.startup["derpy-artillery-range"].value
 derpyArtilleryGun.attack_parameters.cooldown = settings.startup["derpy-artillery-cooldown"].value
