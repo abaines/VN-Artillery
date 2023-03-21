35372c5f3e4286d0463a787f60a622f2cad4f12d
MrDuckFIN <mutanenoskari@gmail.com>
2023-03-21 21:30:26 +0200

    Added basic settings functionality

---
+++ data-updates.lua
@@ -0,0 +1,9 @@
+-- Apply settings
+local derpyArtilleryGun = data.raw["gun"]["derpy-artillery-gun"];
+derpyArtilleryGun.attack_parameters.min_range = settings.startup["derpy-artillery-min_range"].value
+derpyArtilleryGun.attack_parameters.range = settings.startup["derpy-artillery-range"].value
+derpyArtilleryGun.attack_parameters.cooldown = settings.startup["derpy-artillery-cooldown"].value
+
+local derpyArtilleryEntity = data.raw["artillery-turret"]["derpy-artillery"]
+derpyArtilleryEntity.turret_rotation_speed = settings.startup["derpy-artillery-turret-rotation-speed"].value
+-- derpyArtilleryEntity.turn_after_shooting_cooldown = settings.startup["derpy-artillery-cooldown"].value
---
+++ settings.lua
@@ -0,0 +1,31 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "derpy-artillery-range",
+        setting_type = "startup",
+        minimum_value = 0
+        default_value = 21 * 32
+    },
+    {
+        type = "double-setting",
+        name = "derpy-artillery-min_range",
+        setting_type = "startup",
+        minimum_value = 0
+        default_value = 4 * 32
+    },
+    {
+        type = "double-setting",
+        name = "derpy-artillery-cooldown",
+        setting_type = "startup",
+        minimum_value = 0
+        default_value = 200
+    },
+    {
+        type = "double-setting",
+        name = "derpy-artillery-turret-rotation-speed",
+        setting_type = "startup",
+        minimum_value = 0
+        default_value = 0.0001
+        maximum_value = 1
+    }
+})
