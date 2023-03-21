11cdd1bd1ea1495fe55000292d49729dee52ea14
MrDuckFIN <mutanenoskari@gmail.com>
2023-03-21 22:12:19 +0200

    Actually fixed syntax error in settings.lua (Did not save before committing)

--- settings.lua
+++ settings.lua
@@ -3,29 +3,29 @@ data:extend({
         type = "double-setting",
         name = "derpy-artillery-range",
         setting_type = "startup",
-        minimum_value = 0
+        minimum_value = 0,
         default_value = 21 * 32
     },
     {
         type = "double-setting",
         name = "derpy-artillery-min_range",
         setting_type = "startup",
-        minimum_value = 0
+        minimum_value = 0,
         default_value = 4 * 32
     },
     {
         type = "double-setting",
         name = "derpy-artillery-cooldown",
         setting_type = "startup",
-        minimum_value = 0
+        minimum_value = 0,
         default_value = 200
     },
     {
         type = "double-setting",
         name = "derpy-artillery-turret-rotation-speed",
         setting_type = "startup",
-        minimum_value = 0
-        default_value = 0.0001
+        minimum_value = 0,
+        default_value = 0.0001,
         maximum_value = 1
     }
 })
